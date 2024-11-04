936cba5a2164dcbee1e356f6358bfd6bc26410eb
baa <34665120+baa-mb@users.noreply.github.com>
2024-11-04 17:45:48 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,67 @@
+input.onButtonPressed(Button.A, function () {
+    automat = 1
+})
+input.onButtonPressed(Button.B, function () {
+    automat = 2
+})
+input.onButtonPressed(Button.AB, function () {
+    automat = 0
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "A") {
+        basic.showString(receivedString)
+        automat = 1
+        control.raiseEvent(
+            EventBusSource.MICROBIT_ID_BUTTON_A,
+            EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+        )
+    } else if (receivedString == "B") {
+        basic.showString(receivedString)
+        automat = 2
+        control.raiseEvent(
+            EventBusSource.MICROBIT_ID_BUTTON_B,
+            EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+        )
+    } else if (receivedString == "AB") {
+        basic.showString("-")
+        automat = 0
+        control.raiseEvent(
+            EventBusSource.MICROBIT_ID_BUTTON_AB,
+            EventBusValue.MICROBIT_BUTTON_EVT_CLICK
+        )
+    }
+})
+let automat = 0
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+automat = 0
+radio.setGroup(24)
+basic.forever(function () {
+    if (automat == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+        pins.servoWritePin(AnalogPin.P0, 0)
+    } else if (automat == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        pins.servoWritePin(AnalogPin.P0, 180)
+    } else if (automat == 0) {
+        automat = 0
+        pins.servoWritePin(AnalogPin.P0, 90)
+    }
+})
